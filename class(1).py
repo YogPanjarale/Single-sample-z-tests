c25961fb395b908da71f76e9770daee7f129c4ab
YogPanjarale <yog.panjarale@gmail.com>
2021-03-28 18:08:57 +0530

    class complete

--- class(1).py
+++ class(1).py
@@ -15,7 +15,7 @@ print(
 
 # creating the figure
 fig = ff.create_distplot([data], ["Math Score"], show_hist=False)
-
+fig.add_trace(go.Scatter(data,mode="lines",name="Math Score"))
 # calculating for random sample of 1000
 dl = []
 for i in range(0, 1000):
@@ -27,18 +27,20 @@ stdev = stats.stdev(dl)
 #printing values
 print(
     f"Sample - ({len(dl)} Entries) \nMean :{Mean} \nStandard Deviation :{stdev}")
-fig.add_trace(go.Scatter(x=[Mean, Mean], y=[0, 1],
+fig.add_trace(go.Scatter(x=[Mean, Mean],
                          mode="lines", name="1000 sample mean"))
 # calculating for result sample of 100 each
 for i in range(1, 4):
     df = pd.read_csv(f"./data/data{i}.csv")
-    data = df["Math_score"].to_list()
+    data :list= df["Math_score"].to_list()
+    print(data)
     Mean = stats.mean(data)
     stdev = stats.stdev(data)
-    
+    fig.add_trace(go.Scatter(data,mode="lines",name=f"Sample {i}"))
     #printing values
     print(
         f"Sample {i} - ({len(data)} Entries) \nMean :{Mean} \nStandard Deviation :{stdev}")
-    fig.add_trace(go.Scatter(x=[Mean, Mean], y=[0, 1],
+
+    fig.add_trace(go.Scatter(x=[Mean, Mean],y=[0,1],
                              mode="lines", name=f"Result Sample {i}"))
 fig.show()
