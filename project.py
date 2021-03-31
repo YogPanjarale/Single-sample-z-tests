8ad6ec6680492a45378bae97f8265014d640e79b
YogPanjarale <yog.panjarale@gmail.com>
2021-03-31 17:03:59 +0530

    std calculated

--- project.py
+++ project.py
@@ -26,7 +26,7 @@ def takeNSample(list: Iterable, n: int):
 
 
 def setup():
-    ml=[]
+    ml = []
     for i in range(0, 100):
         # taking 30 samples
         samples = takeNSample(rt, 30)
@@ -34,13 +34,43 @@ def setup():
         smean = stats.mean(samples)
         # printing result
         # print(f'Sample No.{i}\nTotal - ({len(samples)} Samples) \nMean: {smean}')
-        #add mean to list
+        # add mean to list
         ml.append(smean)
     return ml
 
-#plotting graph
-def plotGraph(list:Iterable):
-    fig = ff.create_distplot([list],["mean"],show_hist=False)
+
+l = setup()
+std_deviation = stats.stdev(l)
+# findig the standard deviation starting and ending values
+first_std_deviation_start, first_std_deviation_end = mean - \
+    std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean - \
+    (2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean - \
+    (3*std_deviation), mean+(3*std_deviation)
+print("std1", first_std_deviation_start, first_std_deviation_end)
+print("std2", second_std_deviation_start, second_std_deviation_end)
+print("std3", third_std_deviation_start, third_std_deviation_end)
+# plotting graph
+
+
+def plotGraph(list: Iterable):
+    fig = ff.create_distplot([list], ["mean"], show_hist=False)
+    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17],
+                             mode="lines", name="Mean"))
+    fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[
+                  0, 0.17], mode="lines", name="first_std_deviation_start"))
+    fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[
+                  0, 0.17], mode="lines", name="first_std_deviation_end"))
+    fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[
+                  0, 0.17], mode="lines", name="second_std_deviation_start"))
+    fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[
+                  0, 0.17], mode="lines", name="second_std_deviation_end"))
+    fig.add_trace(go.Scatter(x=[third_std_deviation_start, third_std_deviation_start], y=[
+                  0, 0.17], mode="lines", name="third_std_deviation_start"))
+    fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[
+                  0, 0.17], mode="lines", name="third_std_deviation_end"))
     fig.show()
-l=setup()
+
+
 plotGraph(l)
